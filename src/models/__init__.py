093f3fd3d0efe5bc9a281291425a2a862a2331bc
Martin <martin_volk@gmx.net>
2019-05-15 16:50:03 +0200

    Models

---
+++ src/models/__init__.py
@@ -0,0 +1,6 @@
+
+from .lin_regr import LinRegr
+from .nn_regr_2layers import NNLinRegr2Layers
+from .nn_regr_2layers_relu import NNLinRegr2LayersReLU
+
+__all__ = ['LinRegr', 'NNLinRegr2Layers', 'NNLinRegr2LayersReLU']
